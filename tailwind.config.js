b1538c8520b5539c0db282c934c3ac668962c287
Ian Rodrigues <ianrdgs@gmail.com>
2019-11-15 22:21:30 -0300

    Prototyping first design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        'l-24': '0 0 6rem'
+      },
+      fontFamily: {
+        serif: ['Arvo'],
+        mono: ['Source Code Pro']
+      },
+      padding: {
+        '1/2': '0.125rem'
+      }
+    }
   },
   variants: {},
   plugins: [],
